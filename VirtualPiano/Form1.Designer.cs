c0f9801706843e6179f9c762e16484918dd010dc
Niko24x <nikolascohn@gmail.com>
2014-11-24 21:12:35 -0600

    Se termino la programacion que reconoce teclas, los audios en el formato correcto y agregaron los efectos al presionar las teclas, falta crear un loop para que los sonidos suenen constantemente.

--- VirtualPiano/Form1.Designer.cs
+++ VirtualPiano/Form1.Designer.cs
@@ -28,12 +28,276 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnc1 = new System.Windows.Forms.Button();
+            this.btnd1 = new System.Windows.Forms.Button();
+            this.btnf1 = new System.Windows.Forms.Button();
+            this.btne1 = new System.Windows.Forms.Button();
+            this.btnc2 = new System.Windows.Forms.Button();
+            this.btnb1 = new System.Windows.Forms.Button();
+            this.btna1 = new System.Windows.Forms.Button();
+            this.btng1 = new System.Windows.Forms.Button();
+            this.btnb2 = new System.Windows.Forms.Button();
+            this.btna2 = new System.Windows.Forms.Button();
+            this.btng2 = new System.Windows.Forms.Button();
+            this.btnf2 = new System.Windows.Forms.Button();
+            this.btne2 = new System.Windows.Forms.Button();
+            this.btnd2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnc1
+            // 
+            this.btnc1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnc1.Enabled = false;
+            this.btnc1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnc1.Location = new System.Drawing.Point(18, 124);
+            this.btnc1.Name = "btnc1";
+            this.btnc1.Size = new System.Drawing.Size(37, 112);
+            this.btnc1.TabIndex = 0;
+            this.btnc1.TabStop = false;
+            this.btnc1.Text = "C";
+            this.btnc1.UseVisualStyleBackColor = false;
+            this.btnc1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // btnd1
+            // 
+            this.btnd1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnd1.Enabled = false;
+            this.btnd1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnd1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnd1.Location = new System.Drawing.Point(61, 124);
+            this.btnd1.Name = "btnd1";
+            this.btnd1.Size = new System.Drawing.Size(37, 112);
+            this.btnd1.TabIndex = 1;
+            this.btnd1.TabStop = false;
+            this.btnd1.Text = "D";
+            this.btnd1.UseVisualStyleBackColor = false;
+            // 
+            // btnf1
+            // 
+            this.btnf1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnf1.Enabled = false;
+            this.btnf1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnf1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnf1.Location = new System.Drawing.Point(147, 124);
+            this.btnf1.Name = "btnf1";
+            this.btnf1.Size = new System.Drawing.Size(37, 112);
+            this.btnf1.TabIndex = 3;
+            this.btnf1.TabStop = false;
+            this.btnf1.Text = "F";
+            this.btnf1.UseVisualStyleBackColor = false;
+            // 
+            // btne1
+            // 
+            this.btne1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btne1.Enabled = false;
+            this.btne1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btne1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btne1.Location = new System.Drawing.Point(104, 124);
+            this.btne1.Name = "btne1";
+            this.btne1.Size = new System.Drawing.Size(37, 112);
+            this.btne1.TabIndex = 2;
+            this.btne1.TabStop = false;
+            this.btne1.Text = "E";
+            this.btne1.UseVisualStyleBackColor = false;
+            // 
+            // btnc2
+            // 
+            this.btnc2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnc2.Enabled = false;
+            this.btnc2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnc2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnc2.Location = new System.Drawing.Point(319, 124);
+            this.btnc2.Name = "btnc2";
+            this.btnc2.Size = new System.Drawing.Size(37, 112);
+            this.btnc2.TabIndex = 7;
+            this.btnc2.TabStop = false;
+            this.btnc2.Text = "C";
+            this.btnc2.UseVisualStyleBackColor = false;
+            // 
+            // btnb1
+            // 
+            this.btnb1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnb1.Enabled = false;
+            this.btnb1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnb1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnb1.Location = new System.Drawing.Point(276, 124);
+            this.btnb1.Name = "btnb1";
+            this.btnb1.Size = new System.Drawing.Size(37, 112);
+            this.btnb1.TabIndex = 6;
+            this.btnb1.TabStop = false;
+            this.btnb1.Text = "B";
+            this.btnb1.UseVisualStyleBackColor = false;
+            // 
+            // btna1
+            // 
+            this.btna1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btna1.Enabled = false;
+            this.btna1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btna1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btna1.Location = new System.Drawing.Point(233, 124);
+            this.btna1.Name = "btna1";
+            this.btna1.Size = new System.Drawing.Size(37, 112);
+            this.btna1.TabIndex = 5;
+            this.btna1.TabStop = false;
+            this.btna1.Text = "A";
+            this.btna1.UseVisualStyleBackColor = false;
+            // 
+            // btng1
+            // 
+            this.btng1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btng1.Enabled = false;
+            this.btng1.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btng1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btng1.Location = new System.Drawing.Point(190, 124);
+            this.btng1.Name = "btng1";
+            this.btng1.Size = new System.Drawing.Size(37, 112);
+            this.btng1.TabIndex = 4;
+            this.btng1.TabStop = false;
+            this.btng1.Text = "G";
+            this.btng1.UseVisualStyleBackColor = false;
+            // 
+            // btnb2
+            // 
+            this.btnb2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnb2.Enabled = false;
+            this.btnb2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnb2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnb2.Location = new System.Drawing.Point(577, 124);
+            this.btnb2.Name = "btnb2";
+            this.btnb2.Size = new System.Drawing.Size(37, 112);
+            this.btnb2.TabIndex = 13;
+            this.btnb2.TabStop = false;
+            this.btnb2.Text = "B";
+            this.btnb2.UseVisualStyleBackColor = false;
+            // 
+            // btna2
+            // 
+            this.btna2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btna2.Enabled = false;
+            this.btna2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btna2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btna2.Location = new System.Drawing.Point(534, 124);
+            this.btna2.Name = "btna2";
+            this.btna2.Size = new System.Drawing.Size(37, 112);
+            this.btna2.TabIndex = 12;
+            this.btna2.TabStop = false;
+            this.btna2.Text = "A";
+            this.btna2.UseVisualStyleBackColor = false;
+            // 
+            // btng2
+            // 
+            this.btng2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btng2.Enabled = false;
+            this.btng2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btng2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btng2.Location = new System.Drawing.Point(491, 124);
+            this.btng2.Name = "btng2";
+            this.btng2.Size = new System.Drawing.Size(37, 112);
+            this.btng2.TabIndex = 11;
+            this.btng2.TabStop = false;
+            this.btng2.Text = "G";
+            this.btng2.UseVisualStyleBackColor = false;
+            // 
+            // btnf2
+            // 
+            this.btnf2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnf2.Enabled = false;
+            this.btnf2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnf2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnf2.Location = new System.Drawing.Point(448, 124);
+            this.btnf2.Name = "btnf2";
+            this.btnf2.Size = new System.Drawing.Size(37, 112);
+            this.btnf2.TabIndex = 10;
+            this.btnf2.TabStop = false;
+            this.btnf2.Text = "F";
+            this.btnf2.UseVisualStyleBackColor = false;
+            // 
+            // btne2
+            // 
+            this.btne2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btne2.Enabled = false;
+            this.btne2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btne2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btne2.Location = new System.Drawing.Point(405, 124);
+            this.btne2.Name = "btne2";
+            this.btne2.Size = new System.Drawing.Size(37, 112);
+            this.btne2.TabIndex = 9;
+            this.btne2.TabStop = false;
+            this.btne2.Text = "E";
+            this.btne2.UseVisualStyleBackColor = false;
+            // 
+            // btnd2
+            // 
+            this.btnd2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnd2.Enabled = false;
+            this.btnd2.Font = new System.Drawing.Font("Freestyle Script", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnd2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnd2.Location = new System.Drawing.Point(362, 124);
+            this.btnd2.Name = "btnd2";
+            this.btnd2.Size = new System.Drawing.Size(37, 112);
+            this.btnd2.TabIndex = 8;
+            this.btnd2.TabStop = false;
+            this.btnd2.Text = "D";
+            this.btnd2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Freestyle Script", 72F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(69, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(502, 112);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Virtual Production";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 248);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnb2);
+            this.Controls.Add(this.btna2);
+            this.Controls.Add(this.btng2);
+            this.Controls.Add(this.btnf2);
+            this.Controls.Add(this.btne2);
+            this.Controls.Add(this.btnd2);
+            this.Controls.Add(this.btnc2);
+            this.Controls.Add(this.btnb1);
+            this.Controls.Add(this.btna1);
+            this.Controls.Add(this.btng1);
+            this.Controls.Add(this.btnf1);
+            this.Controls.Add(this.btne1);
+            this.Controls.Add(this.btnd1);
+            this.Controls.Add(this.btnc1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnc1;
+        private System.Windows.Forms.Button btnd1;
+        private System.Windows.Forms.Button btnf1;
+        private System.Windows.Forms.Button btne1;
+        private System.Windows.Forms.Button btnc2;
+        private System.Windows.Forms.Button btnb1;
+        private System.Windows.Forms.Button btna1;
+        private System.Windows.Forms.Button btng1;
+        private System.Windows.Forms.Button btnb2;
+        private System.Windows.Forms.Button btna2;
+        private System.Windows.Forms.Button btng2;
+        private System.Windows.Forms.Button btnf2;
+        private System.Windows.Forms.Button btne2;
+        private System.Windows.Forms.Button btnd2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- VirtualPiano/Form1.cs
+++ VirtualPiano/Form1.cs
@@ -15,5 +15,125 @@ namespace VirtualPiano
         {
             InitializeComponent();
         }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString().Equals("J"))
+            {
+                MessageBox.Show(e.KeyCode.ToString());
+            }
+            else
+            {
+                MessageBox.Show("miau");
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //MessageBox.Show(e.KeyCode.ToString());
+            
+            string tecla = e.KeyCode.ToString();
+            switch (tecla)
+            {
+                case "Tab":
+                    this.btnc1.BackColor = System.Drawing.Color.Cyan;
+                        //(Color.Cyan);
+                   // System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\g.wav");
+                    /*SoundEffect soundEffect;
+                    soundEffect = Content.Load<SoundEffect>(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\g.wav");
+                    SoundEffectInstance instance = soundEffect.CreateInstance();*/
+                    //instance.IsLooped = true;
+                    System.Media.SoundPlayer playerc = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\c.wav");
+                    playerc.Play();
+                    break;
+                case "Q":
+                    this.btnd1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerd = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\d.wav");
+                    playerd.Play();
+                    break;
+                case "W":
+                    this.btne1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playere = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\e.wav");
+                    playere.Play();
+                    break;
+                case "E":
+                    this.btnf1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerf = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\f.wav");
+                    playerf.Play();
+                    break;
+                case "R":
+                    this.btng1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerg = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\g.wav");
+                    playerg.Play();
+                    break;
+                case "T":
+                    this.btna1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playera = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\a.wav");
+                    playera.Play();
+                    break;
+                case "Y":
+                    this.btnb1.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerb = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac3\Cortados\b.wav");
+                    playerb.Play();
+                    break;
+                case "U":
+                    this.btnc2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerc2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\c.wav");
+                    playerc2.Play();
+                    break;
+                case "I":
+                    this.btnd2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerd2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\d.wav");
+                    playerd2.Play();
+                    break;
+                case "O":
+                    this.btne2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playere2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\e.wav");
+                    playere2.Play();
+                    break;
+                case "P":
+                    this.btnf2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerf2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\f.wav");
+                    playerf2.Play();
+                    break;
+                case "OemOpenBrackets":
+                    this.btng2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerg2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\g.wav");
+                    playerg2.Play();
+                    break;
+                case "Oem6":
+                    this.btna2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playera2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\a.wav");
+                    playera2.Play();
+                    break;
+                case "Oem5":
+                    this.btnb2.BackColor = System.Drawing.Color.Cyan;
+                    System.Media.SoundPlayer playerb2 = new System.Media.SoundPlayer(@"C:\Users\PeterNíkolas\Documents\Visual Studio 2010\Projects\VirtualPiano\VirtualPiano\media\octavac4\Cortados\b.wav");
+                    playerb2.Play();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            this.btnc1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnd1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btne1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnf1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btng1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btna1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnb1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnc2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnd2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btne2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnf2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btng2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btna2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnb2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+        }
+
+
     }
 }
